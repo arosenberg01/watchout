e27b65e9be0af4f9f5b98d9c73f1a712d9ca2007
arosenberg01 <ansel01@gmail.com>
2015-04-03 16:10:13 -0700

    Draw enemies (circles) and randomize their movement on a set interval

--- watchout.js
+++ watchout.js
@@ -1,2 +1,23 @@
 // start slingin' some d3 here.
 
+var height = 600;
+var width = 1000;
+
+// var enemyData = [];
+
+// for (var i = 0; i < 10; i++) {
+//   var enemy = {};
+//   enemy.cx = Math.floor(Math.random() * width);
+//   enemy.cy = Math.floor(Math.random() * height);
+//   enemy.r = 25;
+//   enemyData.push(enemy);
+// }
+
+var randomMove = function () {
+  d3.select("circle").transition().duration(1000).attr("cx", (Math.floor(Math.random() * width))).attr("cy", (Math.floor(Math.random() * height)));
+}
+
+d3.select('svg').append("circle").attr("cx", 50).attr("cy", 50).attr("r", 25);
+d3.select('svg').append("rect").attr("x", 0).attr("y", 0).attr("width", width).attr("height", height).style({"fill-opacity": 0, "stroke": "black", "stroke-width": 1, "stroke-opacity": 1, "fill": "white"});
+
+setInterval(randomMove, 3000);
