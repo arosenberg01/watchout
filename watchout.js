18a6236f333bee0682e4981ae28f4539520498e3
arosenberg01 <ansel01@gmail.com>
2015-04-04 10:49:15 -0700

    Add collision detection

--- watchout.js
+++ watchout.js
@@ -27,30 +27,45 @@ svg.append("rect")
   .attr("height", height)
   .style({"fill-opacity": 0, "stroke": "black", "stroke-width": 1, "stroke-opacity": 1, "fill": "white"});
 // d3.select('svg').selectAll("circle").data(enemyData).enter().append("circle").attr("cx", Math.floor(Math.random() * width)).attr("cy", Math.floor(Math.random() * height)).attr("r", 25);
+var firstEnemies = generateEnemies();
+
 svg.selectAll("circle")
-  .data(generateEnemies())
+  .data(firstEnemies)
   .enter()
   .append("circle")
   .attr("cx", function(d) { return d.cx})
   .attr("cy", function(d) { return d.cy})
   .attr("r", 10);
 
-var player = svg.selectAll("rect")
-  .data([1,2])
+firstEnemies.push(1);
+
+var player = svg.selectAll("circle")
+  .data(firstEnemies)
   .enter()
-  .append("rect")
-  .attr("x", width / 2)
-  .attr("y", height / 2)
-  .attr("width", 25)
-  .attr("height", 25)
+  .append("circle")
+  .attr("cx", width / 2)
+  .attr("cy", height / 2)
+  .attr("r", 10)
   .style({"fill": "blue", "stroke": "black", "stroke-width": 2})
   .call(d3.behavior.drag().on("drag", move));
 
 function move(d) {
   var x = d3.event.x;
   var y = d3.event.y;
-  d3.select(this).attr("x", x).attr("y", y);
+  d3.select(this).attr("cx", x).attr("cy", y);
 }
+
+function checkCollision(enemy) {
+  var radiusSum = parseFloat(enemy.attr("r")) + parseFloat(player.attr("r"));
+  var xDiff = parseFloat(enemy.attr("cx")) - parseFloat(player.attr("cx"));
+  var yDiff = parseFloat(enemy.attr("cy")) - parseFloat(player.attr("cy"));
+  var separation = (Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    if (separation < Math.pow(radiusSum, 2)) {
+      currentScore = 0;
+    }
+}
+
+var currentScore = 10;
 
 var randomMove = function () {
   d3.select('svg').selectAll("circle").data(generateEnemies()).transition().duration(1000).attr("cx", function(d) { return d.cx}).attr("cy", function(d) { return d.cy});
